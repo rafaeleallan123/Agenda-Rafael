64915616b2d2208885366a60e544372d24ee36fe
rafaeleallan123 <funeldemelo@gmail.com>
2026-07-02 17:25:07 -0300

    Create Favoritavel interface for favoriting functionality

---
+++ src/main/java/Favoritavel.java
@@ -0,0 +1,7 @@
+package br.gov.rn.eejk.model; // Define que esta interface pertence ao pacote model
+
+public interface Favoritavel { // Cria uma interface chamada Favoritavel
+
+    void favoritar(); // Obriga a classe que implementar esta interface a ter o método favoritar
+
+}
